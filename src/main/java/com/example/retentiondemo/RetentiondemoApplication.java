5c513cb825b48c8608df89298c4356dccad35f61
Loylin <935020188@qq.com>
2019-03-31 18:21:48 +0800

    int

---
+++ src/main/java/com/example/retentiondemo/RetentiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.retentiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetentiondemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetentiondemoApplication.class, args);
+    }
+
+}
